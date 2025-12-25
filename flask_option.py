8e1e37abf18d86cc339e93b469471505728b9eb0
Gokce Yuksel <gokceyuksel27@gmail.com>
2025-12-25 22:53:03 +0300

    Implement FLASK_ENV option handling

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,14 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv("FLASK_ENV")
+
+    if env == "development":
+        return "Starting in development mode..."
+    if env == "production":
+        return "Starting in production mode..."
+
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
